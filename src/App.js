f6c4aa7b5c7d8a3fadb4ccb8f0a865dc53fadc9b
gtopinio <mctopinio@up.edu.ph>
2024-05-30 21:21:24 +0800

    update: play around with react fragments

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ const App = () => {
     <div className="App">
       <h1>
         Hello {isNameVisible && name ? name + '!' : 'World!'}
+        {
+          isNameVisible && name ? 
+          <>
+            <p>Name exists!</p>
+          </>
+          :
+          <>
+            <p>Name does not exist!</p>
+          </>
+        }
       </h1>
     </div>
   );
